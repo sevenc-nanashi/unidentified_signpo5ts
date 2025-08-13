ece4329b78c44a345df809292ea5ba03688594a7
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-08-14 08:29:08 +0900

    chore: 音声再生周りをmainに移動

--- src/draw.ts
+++ src/draw.ts
@@ -1,47 +1,14 @@
 import type p5 from "p5";
 import type { State } from "./state.ts";
-import { bg, fg, frameRate, songLength } from "./const.ts";
-import { midi } from "./midi.ts";
-import audio from "./assets/main.mp3?url";
-import { state as capturerState, startCapturer } from "p5-frame-capturer";
+import { bg } from "./const.ts";
 
 const renderers = import.meta.glob("./renderers/*.ts", {
   eager: true,
 }) as Record<string, { draw: (p: p5, state: State) => void }>;
-const audioElement = new Audio(audio);
-audioElement.autoplay = false;
-audioElement.volume = 0.5;
 
-let disposed = false;
-
-let registeredCallback: ((e: KeyboardEvent) => void) | null = null;
 let erroredLastFrame = false;
 export const draw = import.meta.hmrify((p: p5, state: State) => {
-  if (!audioElement.paused && !state.playing) {
-    audioElement.pause();
-  }
-  if (
-    audioElement.paused &&
-    state.playing &&
-    !capturerState.isCapturing &&
-    !disposed
-  ) {
-    audioElement.play();
-    audioElement.currentTime = state.currentFrame / frameRate;
-  }
   try {
-    if (!registeredCallback) {
-      registeredCallback = keydown(p, state);
-      window.addEventListener(
-        "keydown",
-        registeredCallback as (e: KeyboardEvent) => void,
-      );
-    }
-    if (capturerState.isCapturing) {
-      state.currentFrame = capturerState.frameCount;
-    } else if (state.playing) {
-      state.currentFrame = audioElement.currentTime * frameRate;
-    }
     p.background(bg);
     p.noSmooth();
 
@@ -67,53 +34,3 @@ export const draw = import.meta.hmrify((p: p5, state: State) => {
     erroredLastFrame = true;
   }
 });
-
-const keydown = (p: p5, state: State) => (e: KeyboardEvent) => {
-  if (e.key === " ") {
-    state.playing = !state.playing;
-  }
-  if (e.key === "ArrowRight") {
-    audioElement.currentTime = state.currentFrame / frameRate + 5;
-  }
-  if (e.key === "ArrowLeft") {
-    // state.currentFrame -= frameRate * 5;
-    // if (state.currentFrame < 0) {
-    //   state.currentFrame = 0;
-    // }
-    // audioElement.currentTime = state.currentFrame / frameRate;
-    if (audioElement.currentTime > 5) {
-      audioElement.currentTime -= 5;
-    } else {
-      audioElement.currentTime = 0;
-    }
-  }
-  if (e.key === "ArrowUp") {
-    audioElement.volume += 0.1;
-  }
-  if (e.key === "ArrowDown") {
-    audioElement.volume -= 0.1;
-  }
-  if (e.key === "r") {
-    startCapturer(p, {
-      format: "webpLossless",
-      frames: (audioElement.duration + 5) * frameRate,
-      parallelWriteLimit: 0,
-      onFinished: () => {
-        fetch(`https://ntfy.sh/${import.meta.env.VITE_NTFY_TOPIC}`, {
-          method: "POST",
-          body: "All frames are captured!",
-          mode: "no-cors",
-        });
-      },
-    });
-  }
-};
-
-if (import.meta.hot) {
-  import.meta.hot.dispose(() => {
-    audioElement.pause();
-    disposed = true;
-    if (registeredCallback)
-      window.removeEventListener("keydown", registeredCallback);
-  });
-}
--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,92 @@ import "./style.css";
 import { draw } from "./draw.ts";
 import { width, height, frameRate } from "./const.ts";
 import { State } from "./state.ts";
+import audio from "./assets/main.mp3?url";
 // import { attachCapturerUi } from "p5-frame-capturer";
+import { state as capturerState, startCapturer } from "p5-frame-capturer";
+
+const audioElement = new Audio(audio);
+audioElement.autoplay = false;
+audioElement.volume = 0.5;
 
 new p5((p: p5) => {
   const state = new State(0, false);
   p.setup = async () => {
     p.frameRate(frameRate);
     p.createCanvas(width, height);
+    audioElement.load();
   };
 
   p.draw = () => {
+    if (!audioElement.paused && !state.playing) {
+      audioElement.pause();
+    }
+    if (audioElement.paused && state.playing && !capturerState.isCapturing) {
+      audioElement.play();
+      audioElement.currentTime = state.currentFrame / frameRate;
+    }
+    if (capturerState.isCapturing) {
+      state.currentFrame = capturerState.frameCount;
+    } else if (state.playing) {
+      state.currentFrame = audioElement.currentTime * frameRate;
+    }
     draw(p, state);
   };
 
+  p.keyPressed = keydown(p, state);
+
   // attachCapturerUi(p);
 });
+
+function keydown(p: p5, state: State) {
+  return (e: KeyboardEvent) => {
+    if (e.key === " ") {
+      state.playing = !state.playing;
+    }
+    if (e.key === "ArrowRight") {
+      console.log("current time", audioElement.currentTime);
+      audioElement.currentTime += 5;
+      console.log("forward to", audioElement.currentTime);
+    }
+    if (e.key === "ArrowLeft") {
+      // state.currentFrame -= frameRate * 5;
+      // if (state.currentFrame < 0) {
+      //   state.currentFrame = 0;
+      // }
+      // audioElement.currentTime = state.currentFrame / frameRate;
+      console.log(audioElement.currentTime);
+      if (audioElement.currentTime > 5) {
+        audioElement.currentTime -= 5;
+      } else {
+        audioElement.currentTime = 0;
+      }
+      console.log("rewind to", audioElement.currentTime);
+    }
+    if (e.key === "ArrowUp") {
+      audioElement.volume += 0.1;
+    }
+    if (e.key === "ArrowDown") {
+      audioElement.volume -= 0.1;
+    }
+    if (e.key === "r") {
+      startCapturer(p, {
+        format: "webpLossless",
+        frames: (audioElement.duration + 5) * frameRate,
+        parallelWriteLimit: 0,
+        onFinished: () => {
+          fetch(`https://ntfy.sh/${import.meta.env.VITE_NTFY_TOPIC}`, {
+            method: "POST",
+            body: "All frames are captured!",
+            mode: "no-cors",
+          });
+        },
+      });
+    }
+  };
+}
+
+if (import.meta.hot) {
+  import.meta.hot.dispose(() => {
+    audioElement.pause();
+  });
+}
